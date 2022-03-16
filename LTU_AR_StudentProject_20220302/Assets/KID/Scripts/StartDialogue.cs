8bf137ade6e918337042e69a5035056940dd6c1d
KID <50666207+KID421@users.noreply.github.com>
2022-03-16 12:57:45 +0800

    Update 整理專案內容

---
+++ LTU_AR_StudentProject_20220302/Assets/KID/Scripts/StartDialogue.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using Vuforia;
+
+namespace KID
+{
+    public class StartDialogue : MonoBehaviour
+    {
+        [SerializeField]
+        private UnityEvent onClick;
+
+        private VirtualButtonBehaviour vbb;
+
+        private void Start()
+        {
+            vbb = GetComponent<VirtualButtonBehaviour>();
+            vbb.RegisterOnButtonPressed(OnButtonPressed);
+            vbb.RegisterOnButtonReleased(OnButtonReleased);
+        }
+
+        public void OnButtonPressed(VirtualButtonBehaviour vb)
+        {
+            onClick?.Invoke();
+        }
+
+        public void OnButtonReleased(VirtualButtonBehaviour vb)
+        {
+        }
+    }
+}
