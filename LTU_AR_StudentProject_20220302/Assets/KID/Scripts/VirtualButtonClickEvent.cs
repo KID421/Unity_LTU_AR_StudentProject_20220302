feabbd2c8d44fc15a84b27d152378a6d226595e2
KID <50666207+KID421@users.noreply.github.com>
2022-03-23 11:39:37 +0800

    Update 開頭動畫、顯示畫布與發佈 APK

---
+++ LTU_AR_StudentProject_20220302/Assets/KID/Scripts/VirtualButtonClickEvent.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using Vuforia;
+
+namespace KID
+{
+    /// <summary>
+    /// 虛擬按鈕點擊事件
+    /// </summary>
+    public class VirtualButtonClickEvent : MonoBehaviour
+    {
+        [SerializeField, Header("虛擬按鈕點擊事件")]
+        private UnityEvent onClick;
+
+        private VirtualButtonBehaviour vbb;
+
+        private void Start()
+        {
+            vbb = GetComponent<VirtualButtonBehaviour>();
+            vbb.RegisterOnButtonPressed(OnButtonPressed);
+            vbb.RegisterOnButtonReleased(OnButtonReleased);
+        }
+
+        public void OnButtonPressed(VirtualButtonBehaviour vb)
+        {
+            onClick?.Invoke();
+        }
+
+        public void OnButtonReleased(VirtualButtonBehaviour vb)
+        {
+        }
+    }
+}
